aeabcf51ee5c368f87c0fb088eff5ab65ce531fe
Victoria Tillero <victoriatillero6@gmail.com>
2025-04-11 21:34:07 -0400

    finalized lab

--- models/dog.js
+++ models/dog.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose= require("mongoose");
 
-const dogSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    breed:{type: String, required: true},
-    description: String,
-    image: {type: String, required: true},
-})
+const dogSchema=new mongoose.Schema({
+    name: {type: String, required:true},
+    breed: {type: String, required:true},
+    rescueDog: Boolean,
+    pupsonality: String,
+});
 
-const Dog = mongoose.model('Dog', dogSchema)
+const Dog= mongoose.model("Dog", (dogSchema));
 
-module.exports = Dog
+module.exports= Dog
--- server.js
+++ server.js
@@ -1,44 +1,72 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const express = require("express");
-const mongoose= require("mongoose");
-const morgan = require("morgan");
+const express = require('express');
+const mongoose=require('mongoose');
+const methodOverride= require("method-override");
+const morgan= require("morgan")
 
-const app= express();
-const port = process.env.PORT || 3000;
 
-// middleware
-app.use(morgan("dev"));
-app.set("view engine", "ejs");
+const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
-mongoose.connection.on("connected", () => {
-    console.log(`Connected to MongoDB: ${mongoose.connection.name}`)
+mongoose.connection.on("connected", ()=> {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-
-app.get("/", (req,res)=> {
-    res.render("main.ejs");
-});
-
-//connect dog model
+// imports
 const Dog = require("./models/dog.js");
 app.use(express.urlencoded({extended:false}));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
 
-// CREATE ROUTE
+//GET / serves as landing page
+app.get("/", async (req,res) => {
+    res.render("main.ejs")
+})
+
+app.get("/dogs", async (req, res) => {
+    const allDogs = await Dog.find();
+    res.render("dogs/index.ejs", {dogs:allDogs})
+  });
+
 app.get("/dogs/new", (req,res) => {
-    res.render("dogs/new.ejs")
-});
-    // handle submission
-    app.post("/dogs", async (req,res)=> {
-        try {
-            await Dog.create(req.body);
-            res.send(`${req.body.name} submitted!`);
-        } catch (err) {
-            console.log(err);
-            res.send("Error submitting dog")
-        }
-    })
+    res.render("dogs/new.ejs");
+})
 
-app.listen(port, () => {
-    console.log(`Express ready on port ${port}!`)
+app.post("/dogs", async (req,res) => {
+    if (req.body.rescueDog === "on"){
+        req.body.rescueDog= true;
+    } else {
+        req.body.rescueDog = false;
+    }
+    await Dog.create(req.body);
+    console.log(req.body);
+    res.redirect("/dogs")
+})
+app.get("/dogs/:dogId", async (req,res) => {
+    const foundDog = await Dog.findById(req.params.dogId)
+    res.render("dogs/show.ejs", {dog: foundDog});
+})
+
+app.delete("/dogs/:dogId", async (req,res) => {
+    await Dog.findByIdAndDelete(req.params.dogId)
+    res.redirect("/dogs");
+})
+app.get("/dogs/:dogId/edit", async (req,res) => {
+    const foundDog = await Dog.findById(req.params.dogId)
+    res.render("dogs/edit.ejs", {
+        dog:foundDog,
+    });
+})
+app.put("/dogs/:dogId", async (req,res) => {
+    if (req.body.rescueDog === "on") {
+        req.body.rescueDog = true;
+    } else {
+        req.body.rescueDog = false;
+    }
+    await Dog.findByIdAndUpdate(req.params.dogId, req.body);
+    res.redirect(`/dogs/${req.params.dogId}`)
+})
+
+app.listen(3000,() => {
+    console.log('Listening on port 3000')
 });
